c12ec4c333f5bfb46e8793bc4a1119f732a4f478
Tim Lorent <tim@oberon.nl>
2020-02-12 17:32:59 +0100

    Reset formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
 let path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'scrollbooster.min.js',
-        library: 'ScrollBooster',
-        libraryTarget: 'umd',
-        libraryExport: 'default',
-        umdNamedDefine: true,
-        globalObject: 'this',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                },
-            },
-        ],
-    },
-    devServer: {
-        contentBase: path.join(__dirname),
-        clientLogLevel: 'none',
-        open: true,
-    },
-    devtool: 'source-map',
-    optimization: {
-        minimize: true,
-    },
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'scrollbooster.min.js',
+    library: 'ScrollBooster',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    umdNamedDefine: true,
+    globalObject: 'this',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname),
+    clientLogLevel: 'none',
+    open: true,
+  },
+  devtool: 'source-map',
+  optimization: {
+    minimize: true,
+  },
 };
